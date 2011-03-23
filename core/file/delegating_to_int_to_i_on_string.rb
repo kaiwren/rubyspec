c08acb85e86c40e2f6747bacc9eea001204d29e5
Sidu Ponnappa <ckponnappa@gmail.com>
2011-03-24 03:08:38 +0530

    Added Kernel#open which currently only passes on rbx

--- core/file/delegating_to_int_to_i_on_string.rb
+++ core/file/delegating_to_int_to_i_on_string.rb
@@ -5,8 +5,12 @@ describe "delegating String#to_int to String#to_i" do
     String.class_eval{ alias_method :to_int, :to_i }
   end
   
-  it "should not File.readlines hang" do
+  it "should not cause File.readlines hang" do
     File.readlines(__FILE__).should_not be_empty
+  end
+  
+  it "should not cause Kernel#open hang" do
+    open(__FILE__).readlines.should_not be_empty
   end
   
   after(:each) do 
